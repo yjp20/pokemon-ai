644647f512ca51cb7d95362629b1a7aaf726f7ac
suhdavid11@gmail.com <davidsuh@Davids-MacBook-Pro.local>
2019-09-08 23:50:00 +0900

    added mediator js, now work on python part oflookup

--- bots.py
+++ bots.py
@@ -9,10 +9,18 @@ import importlib
 class GameState():
     """
     Stores gamestate as GameState.state, which is then used by the bot. This
-    class also normalizes data to be used in ML purposes.
+    class also normalizes data to be used in ML purporses.
     """
     def __init__(self):
         self.state = {}
+
+        # container for all pokemon data
+        self.party = []
+    def normalize(self, state):
+        """
+        Normalizes state data to be used.
+        """
+        pass
 
     def parse(self, state, normalize):
         """
@@ -23,10 +31,44 @@ class GameState():
             state: state JSON string from Pokemon-Showdown
             normalize: whether or not to normalize data for ML
         """
+
+
+        pokemon_party = json.loads(state)
+        #  self.id = pokemon_party['']
+        # take only the relevant stats for each pokemon, for now
+        for ind, pokemon in enumerate(pokemon_party['side']['pokemon']):
+            self.party.append({})
+            self.party[ind]['id'] = pokemon['ident']
+            self.party[ind]['moves'] = pokemon['moves']
+            self.party[ind]['hp'] = pokemon['condition']
+            self.party[ind]['item'] = pokemon['item']
+            for name in pokemon['stats']:
+                self.party[ind][name] = pokemon['stats'][name]
+
+        # TODO: normalize the party values if chosen to
         if normalize:
-            pass
-        else:
-            self.state = json.loads(state)
+            for poke in self.party:
+                tr = list(map(lambda x: int(x), poke['hp'].split('/')))
+                poke['hp'] = tr[0]/tr[1]
+        
+
+    def update(self, state,  normalize=True):
+            """
+            Updates the gamestate based on the information from STDOUT of the running
+            showdown client, so that the bot can make a decision
+            """
+            
+            data_type = state[0] 
+
+            # upon switch, update the current active pokemon and its hp
+            if data_type == 'switch':
+                tmp = [x for x in self.party if x['id'].split(' ')[1] == state[1].split(' ')[1] and x['id'].split(' ')[0][:-1] in state[1].split(' ')[0][:-1]]
+                print(state[1].split(' ')[0])
+                if len(tmp) > 0:
+                    self.state['active'] = tmp[0]
+            elif data_type == 'turn':
+                print('NOW: TURN {}'.format(state[1]))
+            
 
 class Bot():
     """
---
+++ lib/lookup/buildtb.py
@@ -0,0 +1,4 @@
+# builds table and import information
+# TODO: add check, then lookup information
+def buildTable():
+    pass
