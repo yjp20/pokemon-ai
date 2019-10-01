3f06ae3c9eacd4e8cb049f12ec76d3a099e98bcd
Young Jin Park <youngjinpark20@gmail.com>
2019-10-01 11:04:34 +0900

    Add replay scraper

---
+++ replay_scraper.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+# This module should be run every half a day
+
+import sqlite3
+import requests
+import re
+
+testing = False
+generations = []
+formats = []
+query = 10
+conn = None
+c = None
+
+create_table_string = '''
+CREATE TABLE IF NOT EXISTS replays (
+    name text PRIMARY KEY,
+    gameformat text,
+    log text
+)
+'''
+
+if testing:
+    generations = [1]
+    formats = ['randombattle']
+    query = 4
+    conn = sqlite3.connect('data/testing.db')
+    c = conn.cursor()
+else:
+    generations = [1,2,3,4,5,6,7]
+    formats = ['randombattle', 'ou']
+    conn = sqlite3.connect('data/data.db')
+    c = conn.cursor()
+
+def process_url(url, gameformat):
+    r = requests.get(url)
+    if r.status_code != 200:
+        raise Exception(f'Failed to get {url}')
+    body = str(r.content)
+    games = re.findall('/gen\d[a-z]+-\d+', body)
+    for game in games:
+        print(game)
+        q = c.execute('SELECT EXISTS (SELECT 1 FROM replays where name=? limit 1)', (game,))
+        if q.fetchone()[0] == 0: # if entry doesn't already exist in database
+            print(f'Fetching {game}')
+            sr = requests.get(f'https://replay.pokemonshowdown.com{game}.log')
+            if sr.status_code != 200:
+                raise Exception(f'Failed to get {game}')
+            obj = (game,gameformat,sr.content)
+            c.execute('INSERT INTO replays VALUES (?,?,?)', obj)
+            conn.commit()
+
+def main():
+    c.execute(create_table_string)
+
+    for g in generations:
+        for f in formats:
+            for i in range(1, query+1):
+                fmt = f'gen{g}{f}'
+                url = f'https://replay.pokemonshowdown.com/search?user=&format={fmt}&page={i}&output=html'
+                process_url(url, fmt)
+    conn.close()
+
+if __name__ == '__main__':
+    main()
